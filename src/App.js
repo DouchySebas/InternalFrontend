de37f6701a5668ae8d82836c52c1a8b63279a73d
Sebastian Heng <sh843@uowmail.edu.au>
2024-01-18 21:50:23 +0800

    Update App.js to make URL configurable

--- src/App.js
+++ src/App.js
@@ -2,30 +2,70 @@
 import React, { useState, useEffect } from 'react';
 import './App.css'; // Import the CSS file
 
+class ErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError(error) {
+    console.error('Error caught by error boundary:', error);
+    return { hasError: true };
+  }
+
+  componentDidCatch(error, errorInfo) {
+    console.error('Error caught by error boundary:', error, errorInfo);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      // Return an error message or fallback UI
+      return (
+        <div>
+          <h1>An error occurred.</h1>
+          <p>Please try refreshing the page.</p>
+        </div>
+      );
+    }
+
+    return this.props.children;
+  }
+}
+
+
 function FrontendApp() {
-  const PORT = 3001;
-  const host = `localhost:${PORT}`;
+  const host = process.env.REACT_APP_API_URL;
+  console.log(host);
   var headers = {};
   const [tasks, setTasks] = useState([]);
   const [newTask, setNewTask] = useState({ title: '', description: '' });
   const [showCompleted, setShowCompleted] = useState(true);
 
-  // Filter tasks based on completion status
-  const filteredTasks = showCompleted
+  // Filter tasks based on completion status, (after checking tasks has values)
+  const filteredTasks =
+  showCompleted && Array.isArray(tasks)
     ? tasks
-    : tasks.filter(task => !task.completed);
+    : (Array.isArray(tasks) ? tasks : []).filter(task => !task.completed);
+
 
   // Fetch tasks from the API on component mount
   useEffect(() => {
-    fetch(`http://${host}/api/tasks`)
+    fetchTask()
+  }, []); // Empty dependency array ensures this effect runs once after the initial render
+
+  // Fetch tasks and repopulate list
+  const fetchTask = () => {
+    fetch(`${host}`)
       .then(response => response.json())
       .then(data => setTasks(data))
-      .catch(error => console.error('Error fetching tasks:', error));
-  }, []); // Empty dependency array ensures this effect runs once after the initial render
+      .catch(error => {
+        throw new Error(`Error fetching tasks: ${error}`);
+      });
+  };
 
   // Add a new task to the list
   const addTask = () => {
-    fetch(`http://${host}/api/tasks`, {
+    fetch(`${host}`, {
       method: 'POST',
       mode: 'cors',
       headers: {
@@ -34,8 +74,13 @@ function FrontendApp() {
       body: JSON.stringify(newTask),
     })
       .then(response => response.json())
-      .then(data => setTasks([...tasks, data]))
-      .catch(error => console.error('Error adding task:', error));
+      .then(() => {
+        // Update the task list with the modified task, by re-fetching task list
+        fetchTask()
+      })
+      .catch(error => {
+        throw new Error(`Error fetching tasks: ${error}`);
+      });
 
     // Clear input fields after adding a new task
     setNewTask({ title: '', description: '' });
@@ -43,7 +88,7 @@ function FrontendApp() {
 
   // Mark a task as completed
   const markAsCompleted = (id) => {
-    const url = `http://${host}/api/tasks/${id}`;
+    const url = `${host}/${id}`;
     fetch(url, {
       method: 'PUT',
       mode: 'cors',
@@ -51,62 +96,70 @@ function FrontendApp() {
     })
       .then(response => response.json())
       .then(updatedTask => {
-        // Update the task list with the modified task
-        setTasks(tasks.map(task => (task.id === id ? updatedTask : task)));
+        // Update the task list with the modified task, by re-fetching task list
+        fetchTask()
       })
-      .catch(error => console.error('Error marking task as completed:', error));
+      .catch(error => {
+        throw new Error(`Error fetching tasks: ${error}`);
+      });
   };
 
   return (
-    <div className="task-list-container">
-      <div>
-        <h2>Add New Task</h2>
+    <ErrorBoundary>
+      <div className="task-list-container">
+        <div>
+          <h2>Add New Task</h2>
 
-        {/* Multiline input for Title */}
-        <textarea
-          placeholder="Title"
-          value={newTask.title}
-          onChange={e => setNewTask({ ...newTask, title: e.target.value })}
-          rows="4"
-        />
+          {/* Multiline input for Title */}
+          <textarea
+            placeholder="Title"
+            value={newTask.title}
+            onChange={e => setNewTask({ ...newTask, title: e.target.value })}
+            rows="4"
+          />
 
-        {/* Multiline input for Description */}
-        <textarea
-          placeholder="Description"
-          value={newTask.description}
-          onChange={e => setNewTask({ ...newTask, description: e.target.value })}
-          rows="4"
-        />
-        <br/>
-        {/* Button to Add task */}
-        <button onClick={addTask}>Add Task</button>
+          {/* Multiline input for Description */}
+          <textarea
+            placeholder="Description"
+            value={newTask.description}
+            onChange={e => setNewTask({ ...newTask, description: e.target.value })}
+            rows="4"
+          />
+          <br />
+          {/* Button to Add task */}
+          <button onClick={addTask}>Add Task</button>
+        </div>
+
+        <h2>Task List</h2>
+
+        <div>
+          {/* Button to show all tasks */}
+          <button onClick={() => setShowCompleted(true)}>Show All Tasks</button>
+          {/* Button to show uncompleted tasks */}
+          <button onClick={() => setShowCompleted(false)}>Show Uncompleted Tasks</button>
+        </div>
+
+        <ul className="task-list">
+          {/* Iterate through filtered tasks and display them. If there are no results, or there is a DB error, show No tasks to display instead */}
+          {Array.isArray(filteredTasks) && filteredTasks.length > 0 ? (
+            filteredTasks.map((task) => (
+              <li key={task.id} className={`task-item ${task.completed ? 'completed' : ''}`}>
+                {/* Display task title and description */}
+                <strong>{task.title}</strong> - {task.description}<br />
+                {/* Display task completion status */}
+                Status: {task.completed ? 'Completed' : 'Pending'}
+                <div>
+                  {/* Button to mark a task as completed */}
+                  {!task.completed && <button onClick={() => markAsCompleted(task.id)}>Mark as Completed</button>}
+                </div>
+              </li>
+            ))
+          ) : (
+            <li>No tasks to display</li>
+          )}
+        </ul>
       </div>
-
-      <h2>Task List</h2>
-
-      <div>
-        {/* Button to show all tasks */}
-        <button onClick={() => setShowCompleted(true)}>Show All Tasks</button>
-        {/* Button to show uncompleted tasks */}
-        <button onClick={() => setShowCompleted(false)}>Show Uncompleted Tasks</button>
-      </div>
-
-      <ul className="task-list">
-        {/* Iterate through filtered tasks and display them */}
-        {filteredTasks.map(task => (
-          <li key={task.id} className={`task-item ${task.completed ? 'completed' : ''}`}>
-            {/* Display task title and description */}
-            <strong>{task.title}</strong> - {task.description}<br />
-            {/* Display task completion status */}
-            Status: {task.completed ? 'Completed' : 'Pending'}
-            <div>
-              {/* Button to mark a task as completed */}
-              {!task.completed && <button onClick={() => markAsCompleted(task.id)}>Mark as Completed</button>}
-            </div>
-          </li>
-        ))}
-      </ul>
-    </div>
+    </ErrorBoundary>
   );
 }
 
